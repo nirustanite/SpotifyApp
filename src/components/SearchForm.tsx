d242056960e58e7b388b97c40a5f6742b9f80bb3
Nirupamaa Jaya <nirupamaajaya@Parthasarathys-MacBook-Air.local>
2022-04-10 20:28:40 +0200

    add speech recognition

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Button, Container, Input, InputOnChangeData } from 'semantic-ui-react';
+import { Button, Container, Icon, Input, InputOnChangeData } from 'semantic-ui-react';
 import styled from 'styled-components';
 import SearchDataStore from '../redux/SearchData';
+
+declare global {
+    interface Window { webkitSpeechRecognition: any;  SpeechRecognition: any}
+}
 
 const SearchForm = () => {
 
     const [searchText, setSearchText] = useState<string>('');
     const dispatch = useDispatch();
-    
+
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>, data: InputOnChangeData): void => {
         setSearchText(data.value);
     }
@@ -16,11 +20,31 @@ const SearchForm = () => {
     const handleSearch = () => {
         dispatch(SearchDataStore.actions.getSearchDataRequested(searchText));
     }
-    
+
+    const handleVoice = () => {
+        let SpeechRecognition  =
+            window.webkitSpeechRecognition || window.SpeechRecognition;
+
+        if (typeof SpeechRecognition === 'undefined') {
+            const message: any = document.getElementById('message');
+            message.removeAttribute('hidden');
+            message.setAttribute('aria-hidden', 'false');
+        } else {
+            const recognition = new SpeechRecognition();
+            recognition.start();
+            recognition.onresult = (event : any) => {
+                let word = event.results[0][0].transcript;
+                setSearchText(word);
+            };
+        }
+    }
 
     return (
         <StyledContainer>
-            <StyledInput placeholder='Search for albums tracks and artists' onChange={handleChange}/>
+            <StyledInput placeholder='Search for albums tracks and artists' onChange={handleChange} value={searchText} />
+            <StyledButton icon onClick={handleVoice}>
+                <Icon name='microphone' />
+            </StyledButton>
             <StyledButton primary onClick={handleSearch}>Search</StyledButton>
         </StyledContainer>
     );
@@ -41,3 +65,4 @@ const StyledInput = styled(Input)`
 const StyledButton = styled(Button)`
     margin-left: 20px !important;
 `;
+
